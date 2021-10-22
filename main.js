39a58a3d621278c1b872376593cca147eaa4c900
SAMLPE <said.jasurkhanov@gmail.com>
2021-10-22 12:17:39 +0500

    loops in js intro

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
-function sum (a, b) {
-    var result = a + b;
-    console.log (a + b);
+function checker (a) {
+    let message = String;
+    if(a%2!=0){
+        message = "Is Odd";
+    }else{
+        message = "Is Even";
+    }
+    return message;
     } 
 
-sum(2, 2)
+let names = ['Hell', 'No', 'Fuck oFF'];
+
+for(let i = 0; i < names.length; i++){
+    console.log(names[i])
+}
