372961fb71b3e21bf0033af0abedce00bd1f4674
samlpes <37768942+samlpes@users.noreply.github.com>
2021-11-15 14:39:13 +0500

    adding <p>

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
-function checker (a) {
-    let message = String;
-    if(a%2!=0){
-        message = "Is Odd";
-    }else{
-        message = "Is Even";
-    }
-    return message;
-    } 
-
-let names = ['hello', 'no', 'oFF'];
-
-function reversing(names){
-    let revnames = []
-    for(let i = names.length-1; i>=0; i--){
-        revnames.push(names[i])
-    }
-    return revnames
-
+function checker(a) {
+  let message = String;
+  if (a % 2 !== 0) {
+    message = "Is Odd";
+  } else {
+    message = "Is Even";
+  }
+  return message;
 }
 
-console.log(reversing(names))
+let names = ["hello", "no", "oFF"];
+
+function reversing(names) {
+  let revnames = [];
+  for (let i = names.length - 1; i >= 0; i--) {
+    revnames.push(names[i]);
+  }
+  return revnames;
+}
+
+console.log(reversing(names));
